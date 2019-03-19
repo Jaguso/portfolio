20856b14bc98da26f4d938ea020f009a3f468d38
Jaguso <jagutzo@gmail.com>
2019-03-18 20:08:46 -0600

    adddded background image

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
 import Home from './components/home/Home';
 import Navbar from './components/navbar/Navbar';
 
+
 class App extends Component {
   render() {
     return (
-      <div>
-        <Navbar/>
-        <Home/>
+      <div id="hero">
+        <div id="hero-overlay">
+          <Navbar/>
+          <Home/>
+        </div>
       </div>
     );
   }
