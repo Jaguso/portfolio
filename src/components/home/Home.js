7859a3105b2ee3f824fcb3f335fc77818a80cd7b
Jaguso <jagutzo@gmail.com>
2019-05-28 18:53:10 -0500

    a

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
+import Card from '../card/Card';
 
 class Home extends Component {
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            row1 col1
-          </div>
-          <div className="col">
-            row1 col2
-          </div>
+      <div className="main-container">
+        <p>These are some of my projects:</p>
+        <div>
+          <Card/>
         </div>
 
       </div>
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,22 +1,12 @@
 import React, { Component } from 'react';
+import './Navbar.css';
 
 class Navbar extends Component {
   render() {
     return (
       <nav className="navbar">
-        <a className="navbar-brand" href="/">Personal Portfolio</a>
-        <span>
-          <a className="nav-link" href="#">Resume</a>
-        </span>
-
-        {/* <div className="collapse navbar-collapse">
-          
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <a className="nav-link" href="#">Resume</a>
-            </li>
-          </ul>
-        </div> */}
+        <h1 className="title">Personal Portfolio</h1>
+      
       </nav>
     );
   }
